dbe09041fcdd2205ce46abd72e0bda759062f3f0
이정원 <won31080@gmail.com>
2024-05-19 17:18:48 +0900

    fix: 대화 시스템 버그 수정

--- Assets/Scripts/Character/Player/PlayerController.cs
+++ Assets/Scripts/Character/Player/PlayerController.cs
@@ -40,7 +40,6 @@ public class PlayerController : HumanController
             if (Input.GetKeyDown(currentInteractable.triggerKey))
             {
                 currentInteractable.TriggerInteraction();
-                currentInteractable = null;
             }
         }
 
